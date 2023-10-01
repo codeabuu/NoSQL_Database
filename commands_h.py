5d69f3186cbad9c23a66b6b615e51b73e5e1b7fc
codeabuu <abdulngui718@gmail.com>
2023-10-01 13:34:51 -0700

    updates

--- commands_h.py
+++ commands_h.py
@@ -5,80 +5,81 @@ class CommandHandlers:
     def __init__(self):
         self.DATA = {}
         self.STATS = {}
-def update_stats(command, success):
-    """
-    Update the STATS dict with info about if executing
-    command was a success
-    """
-    if succcess:
-        STATS[command]['success'] += 1
-    else:
-        STATS[command]['error'] += 1
 
-def handle_put(key, value):
-    '''
-    Return a tuple containing True and the message
-    to send back to the client
-    '''
-    DATA[key] = value
-    return (True, 'key[{}] set to [{}]'.format(key, value))
+    def update_stats(self, command, success):
+        """
+        Update the STATS dict with info about if executing
+        command was a success
+        """
+        if success:
+            self.STATS[command]['success'] += 1
+        else:
+            self.STATS[command]['error'] += 1
 
-def handle_get(key):
-    '''
-    Return a tuple containing True if the key exists and the message to send back to client
-    '''
-    if key not in DATA:
-        return(False, 'EROOR: Key [{}] not found'.format(key))
-    else:
-        return(True, DATA[key])
+    def handle_put(self, key, value):
+        '''
+        Return a tuple containing True and the message
+        to send back to the client
+        '''
+        self.DATA[key] = value
+        return (True, 'key[{}] set to [{}]'.format(key, value))
 
-def handle_putlist(key, value):
-    '''the return value inherits from handle_put'''
-    return handle_put(key, value)
+    def handle_get(self, key):
+        '''
+        Return a tuple containing True if the key exists and the message to send back to client
+        '''
+        if key not in self.DATA:
+            return (False, 'ERROR: Key [{}] not found'.format(key))
+        else:
+            return (True, self.DATA[key])
 
-def handle_getlist(key):
-    return_value = exists, value = handle_get(key)
-    if not exists:
-        return return_value
-    elif not isinstance(value, list):
-        return (False, 'ERROR: Key [{}] contains non-list value ([{}]'.format(key, value))
-    else:
-        return return_value
+    def handle_putlist(self, key, value):
+        '''the return value inherits from handle_put'''
+        return self.handle_put(key, value)
 
-def handle_increment(key):
-    '''
-    Return a tuple containing True if the key's value could be incremented and the message to send back to the client
-    '''
-    return_value = exists, value = handle_get(key)
+    def handle_getlist(self, key):
+        return_value = exists, value = self.handle_get(key)
+        if not exists:
+            return return_value
+        elif not isinstance(value, list):
+            return (False, 'ERROR: Key [{}] contains non-list value ([{}]'.format(key, value))
+        else:
+            return return_value
 
-    if not exists:
-        return return_value
-    elif not isinstance(value, int):
-        return (
-                False, 'ERROR: key [{}] containts a non-int value([{}])'.format(key, value)
-                )
-    else:
-        DATA[key] = value + 1
-        return (True, 'key [{}] incremented'.format(key))
+    def handle_increment(self, key):
+        '''
+        Return a tuple containing True if the key's value could be incremented and the message to send back to the client
+        '''
+        return_value = exists, value = self.handle_get(key)
 
-def handle_append(key, value):
-    return_value = exists, list_value = handle_get(key)
+        if not exists:
+            return return_value
+        elif not isinstance(value, int):
+            return (
+                False, 'ERROR: key [{}] contains a non-int value([{}])'.format(key, value)
+            )
+        else:
+            self.DATA[key] = value + 1
+            return (True, 'key [{}] incremented'.format(key))
 
-    if not exists:
-        return return_value
-    elif not isinstance(list_value, list):
-        return (
+    def handle_append(self, key, value):
+        return_value = exists, list_value = self.handle_get(key)
+
+        if not exists:
+            return return_value
+        elif not isinstance(list_value, list):
+            return (
                 False, 'ERROR: Key [{}] contains non-list value ([{}])'.format(key, value)
-                )
-    else:
-        DATA[key].append(value)
-        return (True, 'Key [{}] had value [{}] appended'.format(key, value))
+            )
+        else:
+            self.DATA[key].append(value)
+            return (True, 'Key [{}] had value [{}] appended'.format(key, value))
 
-def handle_delete(key):
-    if key not in DATA:
-        return (False, 'ERROR: Key [{}] not found and could not be deleted'.format(key))
-    else:
-        del DATA[key]
+    def handle_delete(self, key):
+        if key not in self.DATA:
+            return (False, 'ERROR: Key [{}] not found and could not be deleted'.format(key))
+        else:
+            del self.DATA[key]
 
-def handle_stats():
-    return (True, str(STATS))
+    def handle_stats(self):
+        return (True, str(self.STATS))
